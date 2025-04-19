9101eb7c5f48f5d008764b7331906ca613b23a5e
Tanya Singh <66297107+tanya4558@users.noreply.github.com>
2025-04-19 16:05:08 +0530

    vite configjs file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Nik_Renew/', // Ensures relative paths
+  base: '/', // Ensures relative paths
   plugins: [react()],
 })
